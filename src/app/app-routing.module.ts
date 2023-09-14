7064f9a30ba25b685fa89777e897486f8bd8b167
Elattar11 <mohamed.ahmed7350@fmail.com>
2023-09-14 07:08:25 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" ,redirectTo:'home' , pathMatch:"full"},
+  {path:"home" ,component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"portfolio",component:PortfolioComponent},
+  {path:"contact",component:ContactsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent {
+
+  imgPath1:string = "assets/images/poert1.png";
+  imgPath2:string = "assets/images/port2.png";
+  imgPath3:string = "assets/images/port3.png";
+
+  
+
+}
